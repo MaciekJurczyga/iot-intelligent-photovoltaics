68cfa173c50e332db807dbd67ae27e4c895ed7a7
Maciej <maciekjur123@gmail.com>
2025-10-20 12:40:02 +0200

    innit spring project

---
+++ iot-server/src/test/java/com/mjurczyga/iot_server/IotServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mjurczyga.iot_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
