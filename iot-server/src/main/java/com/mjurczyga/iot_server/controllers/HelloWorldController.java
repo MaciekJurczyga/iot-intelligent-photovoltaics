446d6a503d0e761bede05409b16194e09d08ec83
Maciej <maciekjur123@gmail.com>
2025-10-20 14:06:37 +0200

    added docker compose and db infrastracture

--- iot-server/src/main/java/com/mjurczyga/iot_server/controllers/HelloWorldController.java
+++ iot-server/src/main/java/com/mjurczyga/iot_server/controllers/HelloWorldController.java
@@ -2,10 +2,13 @@ package com.mjurczyga.iot_server.controllers;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/api/v1")
+@RestController()
+@RequestMapping("/api/v1")
 public class HelloWorldController {
+
 
     @GetMapping("/hello")
     public ResponseEntity<String> helloWorld() {
